bb81bd9dff843b98ff295ddea18a93fac6efd7d9
Sanju <141401968+sanju9636@users.noreply.github.com>
2023-08-28 08:10:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,19 +14,19 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ρнαηтσм 𝐗 𝐌υʂι𝐂")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "LOFI-MUSIC")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5174492401").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itszshivam/SK_musicbot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/sanju9636/LOFI")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_skshivam")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/fun_here")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/lovezone143")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/neet_k_deewane")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
@@ -75,7 +75,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/af8e81a9a3f788ddafd1c.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/96ccd8e51edee496d1817.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -134,4 +134,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://telegra.ph/file/af8e81a9a3f788ddafd1c.jpg"
+            START_IMG_URL = "https://telegra.ph/file/96ccd8e51edee496d1817.jpg"
